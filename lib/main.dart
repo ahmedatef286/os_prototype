1adbcd712c34a9eac7794166718c5526b957054c
ahmedatef286 <89429256+ahmedatef286@users.noreply.github.com>
2024-05-10 22:53:39 +0300

    setting up

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,15 @@ import 'package:os_prototype/my_app.dart';
 Future<void> main() async {
   runApp(MyApp());
 }
+/*
+      final pyapp = Flython();
+      await pyapp.initialize("python", 'lib/business_logic/python/project.py', false);
+
+//
+      final test = await pyapp.runCommand('list_running_processes');
+      print(test);
+
+//
+      pyapp.finalize();
+
+ */
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -7,9 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey.shade800),
         useMaterial3: true,
       ),
       home: Placeholder(),
